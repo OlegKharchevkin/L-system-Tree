9d3f1c66a5aac87b5374522689d99580e97f91b1
Oleg <harchevknoleg@outlook.com>
2022-11-07 21:58:38 +0300

    stable version

--- UI.py
+++ UI.py
@@ -18,8 +18,8 @@ class UI:
         self.turtle.forward(40)
         self.turtle.write("Click on screen to exit",font=("Arial", 20, "normal"))
     def fullscreen(self):
-    	"""разворачивает окно на полный экран"""
-    	screen = self.turtle.Screen()
-    	screenTk = screen.getcanvas().winfo_toplevel()
-    	screenTk.attributes("-fullscreen", True)
+        """разворачивает окно на полный экран"""
+        screen = self.turtle.Screen()
+        screenTk = screen.getcanvas().winfo_toplevel()
+        screenTk.attributes("-fullscreen", True)
     	
