929023b7e272d337960f40d4313e27b644c01ff8
Oleg <harchevknoleg@outlook.com>
2022-11-08 00:03:18 +0300

    version with multithreadng

--- UI.py
+++ UI.py
@@ -1,6 +1,9 @@
 class UI:
     def __init__(self,turtle):
         self.turtle=turtle
+        screen = self.turtle.Screen()
+        self.w = screen.getcanvas().winfo_screenwidth()
+        self.h = screen.getcanvas().winfo_screenheight()
     def reset(self):
         self.turtle.clear()
         self.turtle.penup()
@@ -9,20 +12,20 @@ class UI:
         self.turtle.pendown()
     def num_of_itr(self,i):
         self.turtle.penup()
-        self.turtle.setpos(-600,250)
-        self.turtle.write(i,font=("Arial", 20, "normal"))
+        self.turtle.setpos(-(self.w/2)+(self.w/16),(self.h/2)-(self.h/9))
+        self.turtle.write(i,font=("Arial", int(self.w/80), "normal"))
         self.turtle.setposition(0,-300)
         self.turtle.seth(90)
         self.turtle.pendown()
     def goodbye_msg(self):
         self.turtle.penup()   
-        self.turtle.setpos(-600,250)
-        self.turtle.right(180)
-        self.turtle.forward(40)
-        self.turtle.write("Click on screen to exit",font=("Arial", 20, "normal"))
+        self.turtle.setpos(-(self.w/2)+(self.w/16),(self.h/2)-(self.h/4.5))
+        self.turtle.write("Click on screen to exit",font=("Arial", int(self.w/80), "normal"))
     def fullscreen(self):
         """разворачивает окно на полный экран"""
         screen = self.turtle.Screen()
         screenTk = screen.getcanvas().winfo_toplevel()
         screenTk.attributes("-fullscreen", True)
-    	
+    def scale(self):
+        screen = self.turtle.Screen()
+        
